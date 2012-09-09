393da787eace319eaad0b41779348fc6f06d8d36
James Ward <jtward.is@gmail.com>
2012-09-09 17:06:21 +0100

    added versioning information

--- src/Archelon.js
+++ src/Archelon.js
@@ -6,7 +6,8 @@
 		DEGREE: 0,
 		RADIAN: 1,
 		DEFAULT_ANGULAR_UNIT: 0,
-		ANGULAR_UNITS: [0, 1]
+		ANGULAR_UNITS: [0, 1],
+		VERSION: '0.1 dev'
 	};
 	
 }((typeof exports === 'undefined') ? window : exports));
